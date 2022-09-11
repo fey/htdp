3a13b930c7a0373dd78daa8df7b7122198bc6a5b
feycot <feycot@gmail.com>
2022-09-12 00:20:00 +0500

    add 20

--- 02/20.rkt
+++ 02/20.rkt
@@ -1,12 +1,15 @@
 #lang htdp/bsl
 (require rackunit)
 
-(define (string-delete string i)
- (string-append
-               (substring string 0 i)
-               (substring string (+ 1 i) (string-length string))))
+(define (string-delete string i) (if (= (string-length string) 0)
+                                  ""
+                                  (string-append
+                                                (substring string 0 i)
+                                                (substring string (+ 1 i) (string-length string)))))
+
 
 (check-equal? (string-delete "hello" 2) "helo")
 (check-equal? (string-delete "h" 0) "")
 (check-equal? (string-delete "hello" 4) "hell")
 (check-equal? (string-delete "hello world" 5) "helloworld")
+(check-equal? (string-delete "" 5) "")
